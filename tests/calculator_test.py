66039b1a3228cd0033f10e7777c828a1dfc36b1b
vg42 <78494078+VG42-njit@users.noreply.github.com>
2021-10-26 14:01:17 -0400

    Update calculator_test.py

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -3,33 +3,35 @@ from calculator.main import Calculator
 
 def test_calculator_result():
     """testing calculator result is 0"""
-    calc = Calculator()
-    assert calc.result == 0
+    calc1 = Calculator()
+    assert calc1.result == 0
 
 def test_calculator_add():
     """Testing the Add function of the calculator"""
     #Arrange by instantiating the calc class
-    calc = Calculator()
+    calc1 = Calculator()
     #Act by calling the method to be tested
-    calc.add_number(1,1)
+    calc1.add_number(1,1)
     #Assert that the results are correct
-    assert calc.result == 2
+    assert calc1.result == 2
 
 def test_calculator_get_result():
     """Testing the Get result method of the calculator"""
-    calc = Calculator()
-    calc.add_number(1)
-    assert calc.get_result() == 2
+    calc2 = Calculator()
+    calc2.add_number(1)
+    assert calc2.get_result() == 2
 
 def test_calculator_subtract():
     """Testing the subtract method of the calculator"""
-    calc = Calculator()
-    calc.subtract_number(0,1)
-    assert calc.get_result() == -1
+    calc3 = Calculator()
+    calc3.subtract_number(0,1)
+    assert calc3.get_result() == -1
+
+
 
 def test_calculater_multiply():
-    calc = Calculater()
-    calc.multiply_number(1,1)
-    assert calc.get_result() == 1
+    calc4 = Calculator()
+    calc4.multiply_number(1,1)
+    assert calc4.get_result() == 1
 
 
