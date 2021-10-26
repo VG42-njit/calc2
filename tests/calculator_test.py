e681959d5562ee359f8f5add2d944f1c3c21b6e3
vg42 <78494078+VG42-njit@users.noreply.github.com>
2021-10-26 14:04:04 -0400

    Update calculator_test.py

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -11,7 +11,7 @@ def test_calculator_add():
     #Arrange by instantiating the calc class
     calc1 = Calculator()
     #Act by calling the method to be tested
-    calc1.add_number(1,1)
+    calc1.add_number(1)
     #Assert that the results are correct
     assert calc1.result == 2
 
@@ -24,14 +24,14 @@ def test_calculator_get_result():
 def test_calculator_subtract():
     """Testing the subtract method of the calculator"""
     calc3 = Calculator()
-    calc3.subtract_number(0,1)
+    calc3.subtract_number(0)
     assert calc3.get_result() == -1
 
 
 
-def test_calculater_multiply():
+def test_calculator_multiply():
     calc4 = Calculator()
-    calc4.multiply_number(1,1)
+    calc4.multiply_number(1)
     assert calc4.get_result() == 1
 
 
