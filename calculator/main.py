a55f45289b86cb377c9eb4c7a694d14ef790b5d3
vg42 <78494078+VG42-njit@users.noreply.github.com>
2021-10-26 15:03:15 -0400

    Update main.py

--- calculator/main.py
+++ calculator/main.py
@@ -4,18 +4,22 @@ class Calculator:
   def __init__(self):
     pass
   #add method - given two numbers, return the addition
-  def add(self, x1, x2):
+  @staticmethod
+  def add(x1, x2):
     return x1 + x2
   #multiply method - given two numbers, return the
   #multiplication of the two
-  def multiply(self, x1, x2):
+  @staticmethod
+  def multiply(x1, x2):
     return x1 * x2
   #subtract method - given two numbers, return the value
   #of first value minus the second
-  def subtract(self, x1, x2):
+  @staticmethod
+  def subtract(x1, x2):
     return x1 - x2
   #divide method - given two numbers, return the value
   #of first value divided by the second
-  def divide(self, x1, x2):
+  @staticmethod
+  def divide(x1, x2):
     if x2 != 0:
       return x1/x2
